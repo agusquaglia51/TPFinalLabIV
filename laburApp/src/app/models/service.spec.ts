8af49dda958e27d19e56e4f0c8ddb21cf80954ac
agus.quaglia51 <agustinquagliarella@gmail.com>
2024-11-04 22:00:37 -0300

    create service model

---
+++ laburApp/src/app/models/service.spec.ts
@@ -0,0 +1,7 @@
+import { Service } from "./service";
+
+describe("Resources", () => {
+  it("should create an instance", () => {
+    expect(new Service()).toBeTruthy();
+  });
+});
---
+++ laburApp/src/app/models/service.ts
@@ -0,0 +1,9 @@
+export class Service {
+  public price?: string = "";
+  public description?: string = "";
+  public mainCategory?: string = "";
+  public secondaryCategory?: string = "";
+  public state: string = "";
+  public department: string = "";
+  public locality: string = "";
+}
